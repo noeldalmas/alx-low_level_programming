af9ab8920979911a80b6a7de219ec4bc520f3f44
ND <oduory52@gmail.com>
2022-03-10 07:02:47 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: 0 since main function takes no arguments
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
