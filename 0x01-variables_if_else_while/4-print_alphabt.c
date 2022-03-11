229e4c2c53744822c0e45132c29a05daf48706d3
Don <oduory52@gmail.com>
2022-03-11 17:00:08 +0400

    print alphabet in lowecase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
