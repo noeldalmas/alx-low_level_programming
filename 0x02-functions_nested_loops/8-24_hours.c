6c88f6b27230e01a12b830ea4e437fb196d78027
ND <oduory52@gmail.com>
2022-03-15 11:14:03 -0700

    delete return

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,6 @@
 
 /**
  * jack_bauer - print every minute of the day
- *
- * Return: Always 0
  */
 void jack_bauer(void)
 {
@@ -15,9 +13,7 @@ void jack_bauer(void)
 		for (min = 00; min <= 59; min++)
 		{
 			_putchar(hour + ':');
-			return (hour);
 			_putchar(min + '0');
-			return (min);
 		}
 	}
 }
