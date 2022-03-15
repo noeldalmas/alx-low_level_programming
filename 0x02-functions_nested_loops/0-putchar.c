85b43815f3451ee63bc834191e34f7c08a596a50
Don <oduory52@gmail.com>
2022-03-15 17:11:57 +0400

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	char k[] = "_putchar";
+
+	for (n = 0; n < 8; n++)
+	{
+		_putchar (n[k]);
+	}
+	_putchar('\n');
+	return (0);
+}
