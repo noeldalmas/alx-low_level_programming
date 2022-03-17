057a41552e90e0af58b5656b405b52b987154d0e
Don <oduory52@gmail.com>
2022-03-17 16:21:21 +0400

    remove {} in if

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,16 @@
 void more_numbers(void)
 {
 	int i;
-	int j;
+	char j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j > 9)
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 		}
-		if (j >= 10)
-		{
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+		_putchar('\n');
 	}
 }
