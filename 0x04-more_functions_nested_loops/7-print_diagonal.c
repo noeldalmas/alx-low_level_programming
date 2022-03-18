3df25ef2b0edf96b429bfb20e394f7615126ad50
ND <oduory52@gmail.com>
2022-03-18 01:40:50 -0700

    reverse equality

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,15 @@
  */
 void print_diagonal(int n);
 {
-	char i, j;
+	int i, j;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j <= i; j++)
 		{
-			if (j == i)
+			if (i == j)
 			{
 				_putchar('\\');
 				_putchar('\n');
