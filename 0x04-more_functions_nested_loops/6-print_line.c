dba275cc3c15a3d6967e4001cf29422b2a74974d
ND <oduory52@gmail.com>
2022-03-17 23:35:46 -0700

    use if condition

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,12 +2,16 @@
 
 /**
  * print_line - prints a straight line
+ * @n: number of underscores
  */
 void print_line(int n)
 {
-	char i;
+	int i;
 
 	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
 		_putchar('_');
+	}
 	_putchar('\n');
 }
