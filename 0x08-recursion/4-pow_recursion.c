86eea56410453575ee852e91d0cc96a3cb5bca95
Don <oduory52@gmail.com>
2022-03-30 13:06:58 +0400

    initial commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - compute power
+ * @x: number to compute
+ * @y: number to raise x to
+ * Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
