8cfe06378f0ac0968b4ba272a396e6fa975f7047
Don <oduory52@gmail.com>
2022-03-30 12:24:39 +0400

    initial commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return length of string
+ * @s: string to use
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\n')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
